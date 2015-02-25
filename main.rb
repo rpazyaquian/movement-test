1d7f93c96bf2cc41ca271f23bef8adc8c38aaf2d
rpazyaquian <rpaz@localytics.com>
2015-02-25 16:06:22 -0500

    displayed character
    added assets
    
    displayed character
    
    centered character

--- main.rb
+++ main.rb
@@ -3,10 +3,20 @@ require 'gosu'
 class GameWindow < Gosu::Window
 	def initialize
 		super(640, 480, false)
+
+		@character = Gosu::Image.new(self, './assets/img/happy.gif')
+		@window_width = 640
+		@window_height = 480
 	end
 	def update
 	end
 	def draw
+		@window_center_x = 640/2
+		@window_center_y = 480/2
+
+		@character_x = @window_center_x - (@character.width / 2)
+		@character_y = @window_center_y - (@character.height / 2)
+		@character.draw(@character_x, @character_y, 0)
 	end
 end
 
